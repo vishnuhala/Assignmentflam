a73136ae68d92146b085214e760e6826bf43023c
vishnuRamsai123 <140895879+vishnuRamsai123@users.noreply.github.com>
2025-11-09 11:15:18 +0530

    Fix static asset serving for Vercel deployment

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ const io = new Server(server, {
 });
 
 // Serve static files from the client directory
-app.use(express.static(path.join(__dirname, 'client')));
+app.use('/', express.static(path.join(__dirname, 'client')));
+app.use('/client', express.static(path.join(__dirname, 'client')));
 
 // Serve index.html for the root route
 app.get('/', (req, res) => {
